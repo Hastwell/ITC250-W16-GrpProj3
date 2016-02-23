e9818e28da381f5be27dea38e3bba7dcf2191e34
AaronHand <spottydj@gmail.com>
2016-02-22 19:04:13 -0800

    added simple feed mechanism

---
+++ wn16/feed/Article.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ahand
+ * Date: 2/18/16
+ * Time: 10:51 AM
+ */
+class Feed
+{
+
+    private $story;
+
+    private $title;
+    private $date;
+    private $body;
+
+    /**
+     * Feed constructor.
+     * @param $story
+     */
+    public function __construct($story)
+    {
+        $this->story = $story;
+
+        $this->title = $story->title;
+        $this->date = $story->pubDate;
+        $this->body = $story->description;
+    }
+
+
+    public function getFeed(){
+
+        return <<<ARTICLE
+
+            <article>
+                <h2>$this->title</h2>
+                <h4>$this->date</h4>
+                <p>$this->body</p>
+            </article>
+
+ARTICLE;
+
+
+    }
+
+
+
+}
---
+++ wn16/feed/feed.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahand
+ * Date: 2/18/16
+ * Time: 11:01 AM
+ */
+
+require_once 'Article.php';
+
+
+
+$url = $_GET['url'];
+
+
+
+    $req = $url;
+    $resp = file_get_contents($req);
+    $xml = simplexml_load_string($resp);
+
+    foreach ($xml->channel->item as $story) {
+        $article = new Feed($story);
+        $article->getFeed();
+    }
