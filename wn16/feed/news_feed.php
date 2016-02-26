dab952875bf5a2b8cec42ab172b080bd5334c4ce
AaronHand <spottydj@gmail.com>
2016-02-25 21:31:16 -0800

    Session Caching for feed data

--- wn16/feed/news_feed.php
+++ wn16/feed/news_feed.php
@@ -9,9 +9,25 @@ require '../inc_0700/config_inc.php'; #provides configuration, pathing, error ha
 require_once 'Article.php';
 
 
+
+if(!isset($_SESSION)){
+    session_start();
+    $_SESSION['time_exp'] = $now + (60 * 15);
+}
+
+$now = time();
+
+if (isset($_SESSION['time_exp']) && $now > $_SESSION['time_exp']){
+    unset($_SESSION['time_exp']);
+    $_SESSION['time_exp'] = $now + (60 * 15);
+}
+
+
 //if( ini_get('allow_url_fopen') ) {
 //    echo "allow_url_fopen is enabled.";
 //}else { echo "allow_url_fopen is disabled.";}
+
+
 get_header(); #defaults to theme header or header_inc.php
 ?>
     <h3 align="center"><?=smartTitle();?></h3>
@@ -22,7 +38,10 @@ get_header(); #defaults to theme header or header_inc.php
 
 
     $req = $_GET['url'];
-    $resp = file_get_contents($req);
+if(!isset($_SESSION['feed'])){
+    $_SESSION['feed'] = file_get_contents($req);
+}
+    $resp = $_SESSION['feed'];
     $xml = simplexml_load_string($resp);
 
     //var_dump($xml);
